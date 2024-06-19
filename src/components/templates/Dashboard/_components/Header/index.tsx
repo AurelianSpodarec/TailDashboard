18c6c83a2102a5f9d86f2a0ccf4a37ac74943016
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2024-06-19 23:55:31 +0100

    feat: added user avatar dropdown

--- src/components/templates/Dashboard/_components/Header/index.tsx
+++ src/components/templates/Dashboard/_components/Header/index.tsx
@@ -1,4 +1,5 @@
 import ModeToggle from "@/components/ModeToggle"
+import UserAvatar from "../UserAvatar"
 
 function SidebarHeader() {
   return (
@@ -6,7 +7,11 @@ function SidebarHeader() {
       <div className="flex h-14 items-center">
 
         Header
+
         <ModeToggle />
+        <div className="ml-auto">
+          <UserAvatar />
+        </div>
       </div>
     </header>
   )
